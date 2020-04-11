72fa21c8436ae805133818e3aa5c3f7bfa68c8fc
djbean3563 <48166566+djbean3563@users.noreply.github.com>
2020-04-10 22:39:21 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dessertImageList = new System.Windows.Forms.ImageList(this.components);
+            this.card0 = new System.Windows.Forms.PictureBox();
+            this.card1 = new System.Windows.Forms.PictureBox();
+            this.card2 = new System.Windows.Forms.PictureBox();
+            this.card3 = new System.Windows.Forms.PictureBox();
+            this.card4 = new System.Windows.Forms.PictureBox();
+            this.card5 = new System.Windows.Forms.PictureBox();
+            this.card6 = new System.Windows.Forms.PictureBox();
+            this.card7 = new System.Windows.Forms.PictureBox();
+            this.card8 = new System.Windows.Forms.PictureBox();
+            this.card9 = new System.Windows.Forms.PictureBox();
+            this.card10 = new System.Windows.Forms.PictureBox();
+            this.card11 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.card0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dessertImageList
+            // 
+            this.dessertImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("dessertImageList.ImageStream")));
+            this.dessertImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.dessertImageList.Images.SetKeyName(0, "cardBack.png");
+            this.dessertImageList.Images.SetKeyName(1, "chocoCake.png");
+            this.dessertImageList.Images.SetKeyName(2, "Cupcake.png");
+            this.dessertImageList.Images.SetKeyName(3, "donut.png");
+            this.dessertImageList.Images.SetKeyName(4, "nutella.png");
+            this.dessertImageList.Images.SetKeyName(5, "OrangeCake.png");
+            this.dessertImageList.Images.SetKeyName(6, "vanillaCake.png");
+            // 
+            // card0
+            // 
+            this.card0.Location = new System.Drawing.Point(25, 10);
+            this.card0.Name = "card0";
+            this.card0.Size = new System.Drawing.Size(220, 250);
+            this.card0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card0.TabIndex = 0;
+            this.card0.TabStop = false;
+            this.card0.Click += new System.EventHandler(this.card0_Click);
+            // 
+            // card1
+            // 
+            this.card1.Location = new System.Drawing.Point(251, 10);
+            this.card1.Name = "card1";
+            this.card1.Size = new System.Drawing.Size(220, 250);
+            this.card1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card1.TabIndex = 1;
+            this.card1.TabStop = false;
+            this.card1.Click += new System.EventHandler(this.card1_Click);
+            // 
+            // card2
+            // 
+            this.card2.Location = new System.Drawing.Point(477, 10);
+            this.card2.Name = "card2";
+            this.card2.Size = new System.Drawing.Size(220, 250);
+            this.card2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card2.TabIndex = 2;
+            this.card2.TabStop = false;
+            this.card2.Click += new System.EventHandler(this.card2_Click);
+            // 
+            // card3
+            // 
+            this.card3.Location = new System.Drawing.Point(703, 10);
+            this.card3.Name = "card3";
+            this.card3.Size = new System.Drawing.Size(220, 250);
+            this.card3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card3.TabIndex = 3;
+            this.card3.TabStop = false;
+            this.card3.Click += new System.EventHandler(this.card3_Click);
+            // 
+            // card4
+            // 
+            this.card4.Location = new System.Drawing.Point(25, 266);
+            this.card4.Name = "card4";
+            this.card4.Size = new System.Drawing.Size(220, 250);
+            this.card4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card4.TabIndex = 4;
+            this.card4.TabStop = false;
+            this.card4.Click += new System.EventHandler(this.card4_Click);
+            // 
+            // card5
+            // 
+            this.card5.Location = new System.Drawing.Point(251, 266);
+            this.card5.Name = "card5";
+            this.card5.Size = new System.Drawing.Size(220, 250);
+            this.card5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card5.TabIndex = 5;
+            this.card5.TabStop = false;
+            this.card5.Click += new System.EventHandler(this.card5_Click);
+            // 
+            // card6
+            // 
+            this.card6.Location = new System.Drawing.Point(477, 266);
+            this.card6.Name = "card6";
+            this.card6.Size = new System.Drawing.Size(220, 250);
+            this.card6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card6.TabIndex = 6;
+            this.card6.TabStop = false;
+            this.card6.Click += new System.EventHandler(this.card6_Click);
+            // 
+            // card7
+            // 
+            this.card7.Location = new System.Drawing.Point(703, 266);
+            this.card7.Name = "card7";
+            this.card7.Size = new System.Drawing.Size(220, 250);
+            this.card7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card7.TabIndex = 7;
+            this.card7.TabStop = false;
+            this.card7.Click += new System.EventHandler(this.card7_Click);
+            // 
+            // card8
+            // 
+            this.card8.Location = new System.Drawing.Point(25, 522);
+            this.card8.Name = "card8";
+            this.card8.Size = new System.Drawing.Size(220, 250);
+            this.card8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card8.TabIndex = 8;
+            this.card8.TabStop = false;
+            this.card8.Click += new System.EventHandler(this.card8_Click);
+            // 
+            // card9
+            // 
+            this.card9.Location = new System.Drawing.Point(251, 522);
+            this.card9.Name = "card9";
+            this.card9.Size = new System.Drawing.Size(220, 250);
+            this.card9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card9.TabIndex = 9;
+            this.card9.TabStop = false;
+            this.card9.Click += new System.EventHandler(this.card9_Click);
+            // 
+            // card10
+            // 
+            this.card10.Location = new System.Drawing.Point(477, 522);
+            this.card10.Name = "card10";
+            this.card10.Size = new System.Drawing.Size(220, 250);
+            this.card10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card10.TabIndex = 10;
+            this.card10.TabStop = false;
+            this.card10.Click += new System.EventHandler(this.card10_Click);
+            // 
+            // card11
+            // 
+            this.card11.Location = new System.Drawing.Point(703, 522);
+            this.card11.Name = "card11";
+            this.card11.Size = new System.Drawing.Size(220, 250);
+            this.card11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card11.TabIndex = 11;
+            this.card11.TabStop = false;
+            this.card11.Click += new System.EventHandler(this.card11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 775);
+            this.Controls.Add(this.card11);
+            this.Controls.Add(this.card10);
+            this.Controls.Add(this.card9);
+            this.Controls.Add(this.card8);
+            this.Controls.Add(this.card7);
+            this.Controls.Add(this.card6);
+            this.Controls.Add(this.card5);
+            this.Controls.Add(this.card4);
+            this.Controls.Add(this.card3);
+            this.Controls.Add(this.card2);
+            this.Controls.Add(this.card1);
+            this.Controls.Add(this.card0);
+            this.Name = "Form1";
+            this.Text = "Memory Game";
+            ((System.ComponentModel.ISupportInitialize)(this.card0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card11)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList dessertImageList;
+        private System.Windows.Forms.PictureBox card0;
+        private System.Windows.Forms.PictureBox card1;
+        private System.Windows.Forms.PictureBox card2;
+        private System.Windows.Forms.PictureBox card3;
+        private System.Windows.Forms.PictureBox card4;
+        private System.Windows.Forms.PictureBox card5;
+        private System.Windows.Forms.PictureBox card6;
+        private System.Windows.Forms.PictureBox card7;
+        private System.Windows.Forms.PictureBox card8;
+        private System.Windows.Forms.PictureBox card9;
+        private System.Windows.Forms.PictureBox card10;
+        private System.Windows.Forms.PictureBox card11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Memory Card game.  Turn over at most two cards at a time.  
+ If you turn over the same two cards, you get a match and the cards can stay face up.
+ Win by matching all the cards. */
+namespace Memory
+{
+    struct Card
+    {
+        public bool faceUp; //true if card is turned over but not matched
+        public int secretPicture; //secret picture on the face of the card; corresponds to dessertImageList Index
+        public bool matched; //true if card has been matched
+        public PictureBox cardPictureBox; //image that the card displays
+    }
+
+    enum SecretPic {Back, Choco, CupCake, Donut, Nutella, Orange, Vanilla};
+
+    public partial class Form1 : Form
+    {
+        //main array of cards
+        Card[] cardArray = new Card[12]; 
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCards();
+            DisplayCards();
+        }
+
+        /*This method creates and deals all the cards for the beginning of the game.*/
+        private void InitializeCards()
+        {
+            for (int i = 0; i < cardArray.Length; i++)
+            {
+                cardArray[i] = new Card(); //initialize card
+                cardArray[i].faceUp = false; //face down
+                cardArray[i].matched = false; //not matched 
+            }
+
+            //Associate cards with their component picture boxes on the form
+            cardArray[0].cardPictureBox = card0;
+            cardArray[1].cardPictureBox = card1;
+            cardArray[2].cardPictureBox = card2;
+            cardArray[3].cardPictureBox = card3;
+            cardArray[4].cardPictureBox = card4;
+            cardArray[5].cardPictureBox = card5;
+            cardArray[6].cardPictureBox = card6;
+            cardArray[7].cardPictureBox = card7;
+            cardArray[8].cardPictureBox = card8;
+            cardArray[9].cardPictureBox = card9;
+            cardArray[10].cardPictureBox = card10;
+            cardArray[11].cardPictureBox = card11;
+
+            //Secret pictures are randomly assigned to the cards in two steps.
+            /*Step One: Get the cards; Create a list of the possible card image index
+             values using the enumerated types SecretPic.  The enumerated types could
+             just be a list of integers but were used to improve readability.  */
+            List<SecretPic> cardPicturesList = new List<SecretPic>();
+            for (int i = 0; i < 2; i++)
+            {
+                cardPicturesList.Add(SecretPic.Choco);
+                cardPicturesList.Add(SecretPic.CupCake);
+                cardPicturesList.Add(SecretPic.Donut);
+                cardPicturesList.Add(SecretPic.Nutella);
+                cardPicturesList.Add(SecretPic.Orange);
+                cardPicturesList.Add(SecretPic.Vanilla);
+            }
+            //Step Two: Deal; Randomly assign secretPictures to the cards.  
+            Random myRand = new Random();
+            int randomNum;
+            for (int i = 0; i < cardArray.Length; i++)
+            {
+                randomNum = myRand.Next(cardPicturesList.Count);
+                cardArray[i].secretPicture = (int) cardPicturesList[randomNum]; //cast to get int from enumerated type
+                cardPicturesList.Remove(cardPicturesList[randomNum]); //remove card from further consideration
+            }
+        }
+
+        /*This method displays all the cards.  It's important to distinguish between
+         cards that are faceUp and cards that are matched.  Matched cards will always be
+         displayed.  On the other hand, a maximum of two faceUp cards will be displayed 
+         at a time.  Matched cards are not faceUp cards (even though we can see their faces).
+        This method also checks if the game has been won.*/
+        private void DisplayCards()
+        {
+            int secretPicture = 0;
+            bool won = true;
+            for (int i = 0; i < cardArray.Length; i++)
+            {
+                if (cardArray[i].matched || cardArray[i].faceUp)
+                {
+                    secretPicture = cardArray[i].secretPicture;
+                    cardArray[i].cardPictureBox.Image = dessertImageList.Images[secretPicture];
+                }
+                else //show the back of the card
+                {
+                    cardArray[i].cardPictureBox.Image = dessertImageList.Images[0];
+                    won = false; //the game is not won if some cards remain undisplayed
+                }
+            }
+            if (won)
+                PlayAgain();
+        }
+
+        //This method asks the user if they want to play again and reintializes the game.
+        private void PlayAgain()
+        {
+            DialogResult dr = MessageBox.Show("Play Again?", "YOU WON!!", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                InitializeCards();
+                DisplayCards();
+            }
+            else
+                this.Close();
+        }
+
+        /*This method checks how many unmatched cards the user is currently viewing.  
+         Returns false if the user is already viewing two unmatched cards.
+         Returns true if the user if viewing 0 or 1 unmatched cards.*/
+        private bool PeeksRemaining()
+        {
+            int faceUp = 0;
+            for (int i = 0; i < cardArray.Length; i++)
+            {
+                if (cardArray[i].faceUp)
+                    faceUp++;
+            }
+            if (faceUp < 2)
+                return true;
+            else //there are already two faceUp cards, so no peaks remaining
+            {
+                for (int i = 0; i < cardArray.Length; i++)
+                {
+                    if (cardArray[i].faceUp)
+                        cardArray[i].faceUp = false; //this is necessary to turn over the unmatched cards
+                }
+            }
+                return false;
+        }
+
+        /*This method checks if the last two cards turned over by the user match.
+         It distinguishes between faceUp cards (i.e. the last two cards turned over by the user)
+         and matched cards (i.e. cards that the  user has previously matched).  If a match is found,
+         the faceUp cards become matched cards.*/
+        private void CheckMatches()
+        {
+
+            int firstCardIndex = 0;  
+            int secondCardIndex = 0;
+            bool firstCardUp = false;
+            bool secondCardUp = false;
+
+            /*This loop captures the main cardArray indices of the card 
+            or cards that are currently face up.*/
+            for (int i = 0; i < cardArray.Length; i++)
+            {
+                if (cardArray[i].faceUp)
+                {
+                    if (firstCardUp)
+                    {
+                        secondCardIndex = i;
+                        secondCardUp = true;
+                    }
+                    else
+                    {
+                        firstCardIndex = i;
+                        firstCardUp = true;
+                    }
+                }
+            }
+            /*If two cards are face up check if they match.
+            It is important to compare the cardArray elements directly as copies will only
+            be aliases and not have the same attribures.*/
+            if (secondCardUp) 
+            {
+                if (cardArray[firstCardIndex].secretPicture == cardArray[secondCardIndex].secretPicture)
+                {
+                    cardArray[firstCardIndex].faceUp = false;
+                    cardArray[secondCardIndex].faceUp = false;
+                    cardArray[firstCardIndex].matched = true;
+                    cardArray[secondCardIndex].matched = true;
+                }
+            }
+        }
+
+        
+
+        /*This method is called when the user clicks a card.  Before revealing the secret picture
+         this method checks to make sure that the user is not already viewing two unmatched cards.
+         Once the secret picture is revealed, this method checks if a match has been found.
+         Then cards are displayed.*/
+        private void TurnOver(int cardIndex)
+        {
+            if (cardArray[cardIndex].faceUp)
+                return; //ignore the click if the card is already face up
+            if (PeeksRemaining())
+            {
+                cardArray[cardIndex].faceUp = true;
+            }
+            CheckMatches();
+            DisplayCards();
+        }
+
+
+        private void card0_Click(object sender, EventArgs e)
+        {
+            TurnOver(0);
+        }
+
+        private void card1_Click(object sender, EventArgs e)
+        {
+            TurnOver(1);
+        }
+
+        private void card2_Click(object sender, EventArgs e)
+        {
+            TurnOver(2);
+        }
+
+        private void card3_Click(object sender, EventArgs e)
+        {
+            TurnOver(3);
+        }
+
+        private void card4_Click(object sender, EventArgs e)
+        {
+            TurnOver(4);
+        }
+
+        private void card5_Click(object sender, EventArgs e)
+        {
+            TurnOver(5);
+        }
+
+        private void card6_Click(object sender, EventArgs e)
+        {
+            TurnOver(6);
+        }
+
+        private void card7_Click(object sender, EventArgs e)
+        {
+            TurnOver(7);
+        }
+
+        private void card8_Click(object sender, EventArgs e)
+        {
+            TurnOver(8);
+        }
+
+        private void card9_Click(object sender, EventArgs e)
+        {
+            TurnOver(9);
+        }
+
+        private void card10_Click(object sender, EventArgs e)
+        {
+            TurnOver(10);
+        }
+
+        private void card11_Click(object sender, EventArgs e)
+        {
+            TurnOver(11);
+        }
+
+        //A print method for quick debugging only
+        private void printCards()
+        {
+            string debugString = "";
+            for (int i = 0; i < cardArray.Length; i++)
+            {
+                debugString += "Card: " + i + " FaceUp? " + cardArray[i].faceUp + " Matched? " +
+                    cardArray[i].matched + "\n";
+            }
+            MessageBox.Show(debugString);
+        }
+    }
+}
